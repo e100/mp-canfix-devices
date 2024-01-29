d51fbe12b628a41987eac0e6478ef8fc74e4aa7e
Eric Blevins <eblevins@kmilearning.com>
2024-01-29 17:53:19 -0500

    Updated switch code with more detailed descriptions of how it works and setup buttons to be one shot by default

--- switches/code.py
+++ switches/code.py
@@ -1,6 +1,38 @@
 # SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
+
+# All buttons are to be momentary, the logic of what happens when 
+# the button is pressed is addressed in pyEFIS and FIX Gateway.
+
+# Keep in mind that this will send the status of up to 40 buttons
+# in a single CAN message. This has presented some challenges that
+# have been dealt with provided things are configured properly.
+
+# Within pyEFIS we have simple, toggle and repeating touchscreen buttons.
+# repeating buttons do not need any special settings, this code will 
+# send True when the button is depressed and send a False when it is released
+
+# toggle and simple buttons do need the special One Shot mode enabled.
+# The problem we have is this device does not know if someone has 
+# pressed the touchscreen button or not and not knowing the state
+# makes it impossible to send the correct state.
+# The one shot mode will send a message with True, then send a message 
+# with False. True will not be sent again until the button is released
+# and pressed again.
+# The FIX Gateway and pyEFIS will treat each True message the same as 
+# if someone has touched the on-screen button.
+#
+# Toggle buttons also require some additional settings in FIX Gateway and 
+# in pyEFIS.
+# In pyEFIS the dbkey used for the button should be setup to output onchange.
+# In FIX Gateway can mapping, the dbkey should be lised in the toggle key 
+# of the switch input configuration.
+#
+# Because simple and toggle buttons are most common I have made the default
+# mode for all buttons to be One Shot. Details about how to setup a 
+# repeating button are detailed below in the section where that is configured.
+
 import board
 import canfix.utils
 from digitalio import DigitalInOut
@@ -34,22 +66,24 @@ COLUMN_PINS = (
 # Each CAN message sends the data for 40 buttons
 # So even if you have just 5 buttons, we still need to send the values for the other 35 bits
 # Make sure you set button_state to a size large enough for all of your buttons
-button_state = [False] * 40
+index_count = int((len(ROW_PINS) * len(COLUMN_PINS)) // 40)
+if index_count == 0: index_count = 1
+button_state = [False] * ( 40 * index_count )
 # Do not change the next line:
 index_changed = [False] * int(len(button_state) // 40)
 
 # One shot buttons only send True one time
 # Once True is sent, it will set the value back to False
-# The user needs to release the button and press it again to send 
+# The user needs to release the button and press it again to send
 # the next True status for that button.
-# To make a button one shot set the array index that matches the
-# button number to True.
 # False might be sent multiple times, but true is only ever sent once per press
-# This is mostly used with pyEFIS 'simple' touchscreen buttons
-# IF you want all buttons to be one shot, change False to True on the next line
-OSB = [False] * len(button_state)
-OSB[0] = True
-
+# This is needed with pyEFIS 'simple' and 'toggle' touchscreen buttons
+# If a paricular button needs to be setup as a repeating button set
+# the button's index to False below
+OSB = [True] * len(button_state)
+# Set button indexes to False for repeat buttons:
+#OSB[0] = False
+#OSB[2] = False
 
 BAUD = 250000
 NODE_SPECIFIC = False #True
@@ -64,7 +98,7 @@ keys = keypad.KeyMatrix(
             column_pins = COLUMN_PINS,
             # Allow 50 msecs to debounce.
             interval=0.080, )
-            
+
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 pixel.brightness = 0.05
@@ -76,11 +110,14 @@ cs.switch_to_output()
 spi = board.SPI()
 
 can_bus = CAN(
-    spi, cs, loopback=True, silent=True, baudrate=BAUD
+    spi, cs, loopback=False, silent=False, baudrate=BAUD
     )  # use loopback to test without another device
 
 def return_data(data_type, data_code, index, button_bits):
-    valueData = canfix.utils.setValue(data_type,button_bits)
+    bytes_array = []
+    for x in range(5):
+        bytes_array.append(button_bits[8 * x:8 * (x + 1)])
+    valueData = canfix.utils.setValue(data_type,bytes_array)
     print(valueData)
     data = bytearray([])
     if NODE_SPECIFIC:
@@ -146,7 +183,7 @@ while True:
                     button_state[one_shot] = False
                     # Reset the one_shot flag
                     one_shot = -1
-                    
+
         except:
             pixel.fill((255, 0, 0))
             success = 0
